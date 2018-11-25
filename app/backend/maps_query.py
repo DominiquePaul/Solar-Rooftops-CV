28cf6094ca560c4a5b01b2120d090d49d4708da7
Francisco Eiras <francisco.girbal.eiras@tecnico.ulisboa.pt>
2018-11-25 04:38:17 +0100

    Complete pipeline

--- app/backend/maps_query.py
+++ app/backend/maps_query.py
@@ -2,6 +2,8 @@ import cv2
 import urllib.request
 from io import BytesIO
 import numpy as np
+import requests
+import math
 
 def gmaps_image(location, zoom=19, size=[300,300]):
     location = location.replace(" ", "+")
@@ -10,3 +12,12 @@ def gmaps_image(location, zoom=19, size=[300,300]):
     image_array = np.asarray(bytearray(requested_url.read()), dtype=np.uint8)
     img = cv2.imdecode(image_array, -1)
     return img
+
+def gmaps_area(location, zoom=19, size=[300,300]): 
+    location = location.replace(" ", "+")
+    response = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyCMCczdtg3LgNkbp-vEMLkhtNKjMkdifVI")
+    resp_json_payload = response.json()
+    lat = resp_json_payload['results'][0]['geometry']['location']['lat']
+    metersPerPx = 156543.03392 * math.cos(lat*math.pi/180)/math.pow(2,zoom)
+    area = size[0]*size[1]*metersPerPx**2
+    return area #in meters squared
